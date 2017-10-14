e24ab055dd38059aa95e6099ff534bd54dab2fef
dimaSkalora <timon2@ukr.net>
2017-10-14 12:08:29 +0300

    2.2. Внедрение в свойства компонентов

---
+++ src/main/java/part_1/head_02/com/springinaction/spring_idol/Instrument.java
@@ -0,0 +1,5 @@
+package part_1.head_02.com.springinaction.spring_idol;
+
+public interface Instrument {
+    void play();
+}
---
+++ src/main/java/part_1/head_02/com/springinaction/spring_idol/Instrumentalist.java
@@ -0,0 +1,32 @@
+package part_1.head_02.com.springinaction.spring_idol;
+
+import part_1.head_02.com.springinaction.spring_idol.exception.PerformanceException;
+
+public class Instrumentalist implements Performer{
+    private String song;
+    private Instrument instrument;
+
+    public Instrumentalist() {
+    }
+
+    @Override
+    public void perform() throws PerformanceException {
+        System.out.print("Playing " + song + " : ");
+        instrument.play();
+    }
+
+    public void setSong(String song) { // Внедрение мелодии
+        this.song = song;
+    }
+
+    public String getSong() {
+        return song;
+    }
+    public String screamSong() {
+        return song;
+    }
+
+    public void setInstrument(Instrument instrument) { // Внедрение инструмента
+        this.instrument = instrument;
+    }
+}
