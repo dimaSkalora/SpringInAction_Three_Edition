31140f3ff2baec1b8c6cc60fda7ef9658f684f23
dimaSkalora <timon2@ukr.net>
2017-10-17 20:12:18 +0300

    4.2.1. Использование аннотации @Autowired

---
+++ src/test/java/part_test_1/head_04/annotation/com/springinaction/spring_idol/autowired/AnnotationConfigTest.java
@@ -0,0 +1,22 @@
+package part_test_1.head_04.annotation.com.springinaction.spring_idol.autowired;
+
+public class AnnotationConfigTest {
+   /*@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("spring-idol_annotation.xml")
+public class AnnotationConfigTest {
+    @Autowired
+    @Qualifier("eddie")
+    private Instrumentalist eddie;
+
+    @Autowired
+    @Qualifier("guitar")
+    private Instrument guitar;
+
+    @Test
+    public void shouldWireWithAutowire() {
+        assertNotNull(eddie.getInstrument());
+        assertEquals(guitar, eddie.getInstrument());
+    }
+}*/
+}
+
