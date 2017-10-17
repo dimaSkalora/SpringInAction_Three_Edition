ab250e9f5809e830eb6806f98e8a9813c466d4d3
dimaSkalora <timon2@ukr.net>
2017-10-17 20:15:54 +0300

    4.3.1. Аннотирование компонентов для автоматического определения

---
+++ src/test/java/part_test_1/head_04/annotation/com/springinaction/spring_idol/component_scan/AnnotationConfigTest.java
@@ -0,0 +1,21 @@
+package part_test_1.head_04.annotation.com.springinaction.spring_idol.component_scan;
+
+public class AnnotationConfigTest {
+    /*RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("spring-idol.xml")
+public class AnnotationConfigTest {
+  @Autowired
+  @Qualifier("eddie")
+  private Instrumentalist eddie;
+
+  @Autowired
+  private Instrument guitar;
+
+  @Test
+  public void shouldWireWithAutowire() {
+    assertNotNull(eddie.getInstrument());
+    assertEquals(guitar, eddie.getInstrument());
+  }
+}*/
+
+}
