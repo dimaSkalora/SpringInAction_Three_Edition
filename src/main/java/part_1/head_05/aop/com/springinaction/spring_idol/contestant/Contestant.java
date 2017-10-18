3e1b8d0042967b21ce84dec971a6f722c47baf84
dimaSkalora <timon2@ukr.net>
2017-10-18 18:24:07 +0300

    5.3.4. Внедрение новых возможностей с помощью аспектов

---
+++ src/main/java/part_1/head_05/aop/com/springinaction/spring_idol/contestant/Contestant.java
@@ -0,0 +1,5 @@
+package part_1.head_05.aop.com.springinaction.spring_idol.contestant;
+
+public interface Contestant {
+    void receiveAward();
+}
---
+++ src/main/java/part_1/head_05/aop/com/springinaction/spring_idol/contestant/GraciousContestant.java
@@ -0,0 +1,7 @@
+package part_1.head_05.aop.com.springinaction.spring_idol.contestant;
+
+public class GraciousContestant implements Contestant {
+    public void receiveAward() {
+        System.out.println("Why, thank you all very much!");
+    }
+}
