d0e2f75df43609a2964e1af5ba45e01cdffc3d45
dimaSkalora <timon2@ukr.net>
2017-10-18 14:22:30 +0300

    5.3.2. Объявление советов, выполняемых и до, и после

---
+++ src/main/java/part_1/head_05/aop/com/springinaction/spring_idol/AroundAudience.java
@@ -0,0 +1,20 @@
+package part_1.head_05.aop.com.springinaction.spring_idol;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class AroundAudience {
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+        try {
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+            long start = System.currentTimeMillis(); // Перед выступлением
+            joinpoint.proceed(); // Вызов целевого метода
+            long end = System.currentTimeMillis(); // После выступления
+            System.out.println("CLAP CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start)
+                    + " milliseconds.");
+        } catch (Throwable t) {
+            System.out.println("Boo! We want our money back!");
+        }
+    }
+}
